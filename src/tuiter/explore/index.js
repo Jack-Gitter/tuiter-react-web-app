519a6debd39602e1f75500c369d0e6356f0e60e0
Jack Gitter <gitter.j@husky.neu.edu>
2023-02-21 18:03:48 -0500

    nice

--- src/tuiter/explore/index.js
+++ src/tuiter/explore/index.js
@@ -6,10 +6,10 @@ const ExploreComponent = () => {
       <>
         <div className="row">
           <div className="col-11 position-relative">
-            <input placeholder="Search Tuiter"
-                   className="form-control rounded-pill ps-5"/>
             <i className="bi bi-search position-absolute
                        wd-nudge-up"></i>
+            <input placeholder="Search Tuiter"
+                   className="form-control rounded-pill ps-5"/>
           </div>
           <div className="col-1">
             <i className="wd-bottom-4 text-primary float-end bi
