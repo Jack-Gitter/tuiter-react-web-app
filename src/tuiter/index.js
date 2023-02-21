c0c66058ddc93624057a569fb6bad42ec8d7f4fa
Jack Gitter <gitter.j@husky.neu.edu>
2023-02-21 17:48:57 -0500

    okok

--- src/tuiter/index.js
+++ src/tuiter/index.js
@@ -2,13 +2,13 @@ import React from "react";
 import Nav from "../nav";
 import NavigationSidebar from "./navigation-sidebar";
 import WhoToFollowList from "./who-to-follow-list";
+import PostSummaryItem from "./post-summary-list/post-summary-item";
+import PostSummaryList from "./post-summary-list";
 
 function Tuiter() {
   return(
    <div>
      <Nav/>
-     <NavigationSidebar active="home"/>
-     <WhoToFollowList/>
      <h1>Tuiter</h1>
    </div>
   );
